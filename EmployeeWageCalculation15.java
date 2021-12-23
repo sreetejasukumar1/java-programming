c7eb07b5b5f40acc7d2ebeda17d7b0420dd8677b
sreetejasukumar <55345889+sreetejasukumar@users.noreply.github.com>
2021-12-23 16:40:55 +0530

    employee wage calculation

---
+++ EmployeeWageCalculation15.java
@@ -0,0 +1,31 @@
+public class  EmployeeWageCalculation15 {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+
+   public static void main(String[] args){
+
+         int empHrs = 0, empWage = 0, totalEmpWage = 0;
+         for (int days = 0; days < NUM_OF_WORKING_DAYS; days++) {
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+         switch (empCheck){
+            case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+            case IS_FULL_TIME:
+                        empHrs=8;
+                        break;
+            default:
+               empHrs=0;
+         }
+
+         empWage = empHrs * EMP_RATE_PER_HOUR;
+         totalEmpWage += empWage;
+         System.out.println("Emp Wage:" + empWage);
+         }
+         System.out.println("Total Emp Wage: " + totalEmpWage);
+   }
+}
+
